c092c774878329632f1688cab8bfa2a3b96ae95a
Samuel Onyijne <samuelord1@gmail.com>
2020-06-09 00:22:53 +0100

    added bootstrap and updated example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-const landingPage = {
-  mainNode: document.querySelector('div[data-app]'),
-  display: function(html) {
-    this.mainNode.innerHTML = `${html}`;
-  },
-  html: `<div class="landing-main"><p>Welcome to falcon frontend landing page</p></div>`,
-  run: () => landingPage.display(landingPage.html)
-};
-landingPage.run();
+import "bootstrap";
+import page from "./landing";
+import "./scss/style.scss";
+
+page.init();
