aa3ac5abe7b3f0976841ed9d1a08100f5924d17c
Babscode <scotteyawo@gmail.com>
2020-06-10 15:15:36 +0100

    Working on the html
    Added to the js,css and img folders
    Created rooms.html

---
+++ design-two/assets/js/main.js
@@ -0,0 +1,17 @@
+$('.reviews-row').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: true,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
