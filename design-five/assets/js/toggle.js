66cf4ff8dc73277a149ff79dfa79751a28459ae2
AndikanAffiah <andikanaffiah@gmail.com>
2020-06-11 20:49:05 +0000

    Design Five 'Updated and optimized the navigation bar and footer for responsiveness'

--- design-five/assets/js/toggle.js
+++ design-five/assets/js/toggle.js
@@ -1,9 +1,17 @@
-// Contact Page Navbar 
-let navBar = document.getElementById("contact-us-nav");
-let navUl = document.getElementById("contact-us-ul");
-let hamburgerMenu = document.getElementById("hamburger");
 
-hamburgerMenu.addEventListener("click", function() {
-  navBar.classList.toggle("active");
-  navUl.classList.toggle("active");
-});
+function toggleMe(x) {
+  x.classList.toggle("change");
+  // console.log(x)
+  x.nextElementSibling.classList.toggle("dropdown");
+  x.nextElementSibling.firstElementChild.classList.toggle("dropdown-content");
+  x.nextElementSibling.firstElementChild.classList.toggle("show");
+}
+
+// window.onclick = (e)=>{
+//   if(!e.target.matches("#menu")){
+//     let menu = document.querySelector("#menu");
+//     if(menu.classList.contains("change")){
+//       menu.nextElementSibling.firstElementChild.classList.remove("show")
+//     }
+//   }
+// };
