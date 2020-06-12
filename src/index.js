f85afd886bd4adfd749463f586723f795a821040
Samuel Onyijne <samuelord1@gmail.com>
2020-06-12 01:07:47 +0100

    apdated landing page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import "bootstrap";
+import "fontawesome";
 import page from "./landing";
 import "./assets/scss/landing.scss";
 
-page.render();
+const initApp = async () => {
+  const header = document.querySelector('header[data-header]');
+  if (!header) return;
+
+  await page.display(header, '<h1>Discover our Demos</h1>');
+  await page.render();
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
